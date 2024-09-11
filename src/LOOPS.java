abca5c841cc50da7487ac3509870434da911372f
nitinnkr99 <nitinnkr99@gmail.com>
2024-09-11 10:45:04 +0530

    new repo created

---
+++ src/LOOPS.java
@@ -0,0 +1,19 @@
+public class LOOPS {
+    public static void main(String[] args) {
+        /*
+        LOOPS :
+        basic Syntax of for loop in java is cpp.
+
+        for (initialization ; condition ; increment / decrement ){
+            body of loop;
+            }
+
+         */
+
+        //Q : print all natural no.s from 1 to 10.
+
+        for (int num = 1;num<=10;num++){
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        CONDITIONALS AND LOOPS :
+        SYNTAX OF if-else :
+        if(boolean condition){
+            body of if executes when true;
+        }
+        else {
+            body of else executes when if is false;
+        }
+         */
+        System.out.println("ENTER the SALARY  : ");
+        Scanner inp = new Scanner(System.in);
+        int sal = inp.nextInt();
+        if (sal > 10000){
+            sal = sal + 3000;
+        }
+        else {
+            sal = sal + 1500;
+        }
+        System.out.println(sal);
+
+    }
+}
