2d1098e801b2d8b4ffbc49dc22f4f3885ab32452
nitinnkr99 <nitinnkr99@gmail.com>
2024-09-11 12:19:09 +0530

    -

--- src/LOOPS.java
+++ src/LOOPS.java
@@ -4,6 +4,8 @@ public class LOOPS {
         LOOPS :
         basic Syntax of for loop in java is same as cpp.
 
+        for loop is used when we know exactly how many times we need to run the loop
+        for example print ten numbers , sum of first 10 natural numbers .
         for (initialization ; condition ; increment / decrement ){
             body of loop;
             }
@@ -15,5 +17,27 @@ public class LOOPS {
         for (int num = 1;num<=10;num++){
             System.out.print(num + " ");
         }
+        System.out.println("\n");
+
+        System.out.println("below is the output of while loop");
+
+        /*
+        while loop is used when we don't exactly know how many iterations need to be done
+        such that run the loop until the user presses X or something ; so here we dont know how many
+        times exactly loop will be running u just provide the condition in the while loop and if it becomes
+        false the loop will terminate itself.
+        syntax for WHILE loop :
+            while (condition) {
+                body;
+                increment/decrement;
+             }
+         */
+        //Q : print first 5 natural numbers
+        int n=1;
+        while (n<=5){
+            System.out.println(n);
+            n++;
+        }
+
     }
 }
