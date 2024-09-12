8c8524943e9497795aff5d154c4a3b6febeb7598
nitinnkr99 <nitinnkr99@gmail.com>
2024-09-12 13:11:41 +0530

    -

---
+++ src/Fibonacci.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+//fibonacci is like 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13 , 21....
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("enter the no, of terms : ");
+        int n = inp.nextInt();
+        int p = 0;
+        int q =1;
+
+        int count = 2;
+
+
+    }
+}
