ebf6c336204396d26b239b9344ef02228feabb16
nitinnkr99 <nitinnkr99@gmail.com>
2024-09-14 10:52:46 +0530

    -

---
+++ src/ANSWERS/Palindrome.java
@@ -0,0 +1,12 @@
+package ANSWERS;
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        String word  = read.next().trim();
+
+        while(true){
+            if ()
+        }
+    }
+}
