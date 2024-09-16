fee9f43c3a268be7c171bf78d31ab5ace9dd59c4
nitinnkr99 <nitinnkr99@gmail.com>
2024-09-16 11:12:45 +0530

    -PassByValue basics

---
+++ src/PassByValue.java
@@ -0,0 +1,28 @@
+public class PassByValue {
+    public static void main(String[] args) {
+//in java when an argument is given then a copy of reference variable is passed into the function and they both point
+        //at the same object (or different maybe totally dependent on java in case of primitive data-types)
+
+
+    String name = "noob";
+    changeName(name); //output : noob
+    System.out.println(name); //it should've changed the value of name variable but it didn't actually
+    } //why doesn't the value changed??
+
+
+
+    static String changeName(String x){
+        //this x will be getting a copy of name reference variable
+        //but here we're not changing that value by this below
+        x = "another noob";
+        // instead it creates a new object in heap memory and x starts pointing towards
+        return x;
+        //"another noob" and name still points towards the "noob" object in heap memory so there's no change in name at all
+    }
+
+
+
+    //NOTE:
+    //java only has pass by value feature and not pass by reference that cpp and c has
+    //only the value of a reference variable is passed as argument in a function
+}
