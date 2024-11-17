7b0633bbef14dfeb290caf07b5ec9000e75059c2
nitinnkr99 <nitinnkr99@gmail.com>
2024-11-17 22:26:43 +0530

    -

---
+++ src/BinarySearch.java
@@ -0,0 +1,16 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        /*
+            binary search algorithm is a faster way to find a element in a sorted array.
+            Step-1 : In the given sorted array find the middle element.
+            Step-2 : compare the key with middle element.
+            Step-3 : If the key is smaller , element is in first half , search in left part.
+            Step-4 : If the key is larger , element is in second half , search in right.
+            Step-5 : Make the starting and ending point in the array where element is and again repeat from step-1.
+
+              mid = (Start pt. + end pt.)/2
+              best case = O(1)
+              worst case = O(log n)
+         */
+    }
+}
